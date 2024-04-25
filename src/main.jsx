db6d139b2bdb4246a3b9e54dbf95b6220bda5755
Gilbert Haro <gilbertharocode@gmail.com>
2024-04-24 18:13:28 -0700

    some base project folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Components from './pages/Components';
 
-// import App from './App.jsx';
+import App from './App.jsx';
 import './index.css';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Link to='/'> Home </Link>
-      <Link to='/about'> About </Link>
-      <Link to='/elements'> Elements </Link>
-      <Link to='/components'> Components </Link>
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/components' element={<Components />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -4,6 +4,12 @@ import Crumb from '../Components/Crumb';
 const Components = () => {
   return (
     <div>
+      <h2>Component Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        laboriosam impedit doloribus eos quo ullam nulla totam itaque est
+        facilis.
+      </p>
       <Breadcrumbs>
         <Crumb href='/'>Home</Crumb>
         <Crumb href='/living'>Living Room</Crumb>
